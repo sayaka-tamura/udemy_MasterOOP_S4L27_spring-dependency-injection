5ac27a3595c37dd8c55cbf3d3c2c717695c10486
sayaka-tamura <arccycle@gmail.com>
2019-04-27 20:00:32 +0900

    Revised applicationContext.xml and used bean at App.java

--- src/main/java/section4/dependencyInjection/clients/App.java
+++ src/main/java/section4/dependencyInjection/clients/App.java
@@ -2,16 +2,24 @@
  * @author : Sayaka Tamura
  * Apr 27, 2019
  * Udemy Master Object Oriented Design in Java
- * Section 4 Lecture 26 "Dependency Injeciton Part I"
+ * Section 4 Lecture 27 "Dependency Injeciton Part II"
  */
 package section4.dependencyInjection.clients;
 
-import section4.dependencyInjection.parts.SmallEngine;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import section4.dependencyInjection.vehicles.Vehicle;
 
 public class App {
 	public static void main(String[] args) {
-		Vehicle raceCar = new Vehicle(new SmallEngine(100)); // proper Dependency Injection Part : new SmallEngine(100)
-		raceCar.crankIgnition();
+		// Section4 Lecture 26
+//		Vehicle raceCar = new Vehicle(new SmallEngine(100)); // proper Dependency Injection Part : new SmallEngine(100)
+//		raceCar.crankIgnition();
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		Vehicle car1 = (Vehicle) context.getBean("mrBeansCarLarge");
+		car1.crankIgnition();
 	}
 }
